b633bbde40bfd7323fc880c22c30f5b06f2079ba
JulliaW <weberjullia44@gmail.com>
2023-05-26 14:47:34 -0300

    Adicionando projeto

---
+++ animes/Controllers/AnimeController.cs
@@ -0,0 +1,62 @@
+﻿using animes.Data;
+using animes.Models;
+using JikanDotNet;
+using Microsoft.AspNetCore.Mvc;
+using System.Text;
+
+namespace animes.Controllers
+{
+    public class AnimeController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        IJikan jikan = new Jikan();
+        public AnimeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Search(string searchQuery)
+        {
+            var buscaAnime = await jikan.SearchAnimeAsync(searchQuery);
+
+            var searchLog = new AnimeSearchLog
+            {
+                NomeAnime = searchQuery,
+                DataHora = DateTime.Now
+            };
+
+            _context.animeSearchLogs.Add(searchLog);
+            _context.SaveChanges();
+
+            return View("Search", buscaAnime.Data);
+        }
+
+        public IActionResult GenereteLogsFile()
+        {
+            var logs = _context.animeSearchLogs.ToList();
+            var fileContent = GenerateLogsFileContent(logs);
+            var fileName = "anime_logs.txt";
+
+            return File(Encoding.UTF8.GetBytes(fileContent), "text/pain", fileName);
+        }
+
+        private string GenerateLogsFileContent(List<AnimeSearchLog> logs)
+        {
+            var sb = new StringBuilder();
+
+            sb.AppendLine("Anime Search Logs");
+            sb.AppendLine("======================================");
+
+            foreach (var log in logs)
+            {
+                sb.AppendLine($"Nome do Anime: {log.NomeAnime}");
+                sb.AppendLine($"Data e Hora da consulta: {log.DataHora}");
+                sb.AppendLine("--------------------------------------");
+            }
+
+            return sb.ToString();
+        }
+
+    }
+}
